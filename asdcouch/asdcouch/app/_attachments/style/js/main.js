2e2adc4628193bf897348bc48cf22de6f787048d
Scott Waite <scomttwaite@gmail.com>
2013-04-26 10:45:20 -0400

    Updated js

--- asdcouch/asdcouch/app/_attachments/style/js/main.js
+++ asdcouch/asdcouch/app/_attachments/style/js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
-	console.log('I am ready!');
-	
-});
+    $.ajax({
+        "url": "_view/employees",
+        "dataType": "json",
+        "success": function(data) {
+            $.each(data.rows, function(index, program){
+                var name = employee.value.name;
+                var number = employee.value.number;
+                $('#employeelist').append(
+                    #('<li>').append(
+                        $('<a>').attr("href", "#")
+                            .text(title)
+                        )
+                    );
+
+            });
+            $('#programlist').listview('refresh');
+        }
+    });
+
