c832c8a0fee2b6e16abd229e68959f9a4c5341c0
Scott Waite <scomttwaite@gmail.com>
2013-05-01 11:03:16 -0400

    Update Attachments

--- Week 3/Project/main.js
+++ Week 3/Project/main.js
@@ -2,6 +2,30 @@
 // ASD Term 1304 
 $(document).ready(function () {
 
+    $.ajax({
+        "url": '/asddirectory/_all_docs?include_docs=true&start_key="name:"&end_key="name:zzz"',
+        "dataType": "json",
+        "success": function(data) {
+            $.each(data.rows, function(index, name){
+                var name = employee.doc.name;
+                var number = employee.doc.number;
+                $('#employeelist').append(
+                    $('<li>').append(
+                        $('<a>').attr("href", "#")
+                            .text(title)
+                    )                
+                );              
+            });
+            $('#employeelist').listview('refresh');
+        }
+    });
+
+
+
+
+
+
+
 
 
     $(function () {
